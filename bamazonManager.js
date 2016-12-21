f398dd5e9658ff2410ac69dd385126ebe6f73459
Julian Khatibi <ajkhatibi@gmail.com>
2016-12-20 18:49:03 -0800

    part 2

---
+++ bamazonManager.js
@@ -0,0 +1,125 @@
+var inquirer = require('inquirer');
+var mysql = require('mysql');
+			var connection = mysql.createConnection({
+			  host     : 'localhost',
+			  user     : 'root',
+			  password : 'Khatibi1!2',
+			  database : 'bamazon'
+			});
+			connection.connect(function(err){
+				if (err) throw err;
+				mainSearch();
+			});
+
+var mainSearch = function(){
+	inquirer.prompt([
+			{
+				name: 'mainSearch',
+				message: 'Select List of Menu Options',
+				type: 'list',
+				choices: ['View Products for Sale', 'View Low Inventory', 'Add to Inventory', 'Add New Product']
+		
+			}
+		]).then(function(answers){
+			switch (answers.mainSearch){
+				case 'View Products for Sale':
+			var mysql = require('mysql');
+			var connection = mysql.createConnection({
+			  host     : 'localhost',
+			  user     : 'root',
+			  password : 'Khatibi1!2',
+			  database : 'bamazon'
+			});
+			connection.connect();
+			connection.query('SELECT * FROM products', function(err, rows, fields) {
+			  if (err) throw err;
+			 
+			  console.log('The solution is: ', rows);
+			});
+			 
+			connection.end(function(err){
+				if(err) throw err
+					mainSearch();
+			});
+					break;
+				case 'View Low Inventory':
+					lowInventory();
+					break;
+				case 'Add to Inventory':
+					addInventory();
+					break;
+				case 'Add New Product':
+					addNewInventory();
+					break;
+			}
+		})
+}
+
+var lowInventory = function(){
+	var mysql = require('mysql');
+			var connection = mysql.createConnection({
+			  host     : 'localhost',
+			  user     : 'root',
+			  password : 'Khatibi1!2',
+			  database : 'bamazon'
+			});
+			connection.connect();
+			connection.query('SELECT * FROM products WHERE stock_quantity <= 10', function(err, rows, fields) {
+			  if (err) throw err;
+			 
+			  console.log('The solution is: ', rows);
+			});
+			 
+			connection.end(function(err){
+				if (err) throw err
+					mainSearch();
+			});
+		}
+var addInventory = function(){
+	var mysql = require('mysql');
+			var connection = mysql.createConnection({
+			  host     : 'localhost',
+			  user     : 'root',
+			  password : 'Khatibi1!2',
+			  database : 'bamazon'
+			});
+			connection.connect(function(err){
+				if(err) throw err
+					inquirer.prompt([
+							{
+								name: 'addInventory',
+								message: 'What would you like to add to the inventory?',
+								type: 'list',
+								choices: ['snuggie','boombox', 'ipod']
+
+							},
+							{
+								name: 'addInventory2',
+								message: 'How many would you like to add?',
+								type: 'list',
+								choices: ['1','10', '100', '1000']
+							}
+						]).then(function(answers){
+							var addInventory = answers.addInventory;
+							var addInventory2 = answers.addInventory2;
+
+							connection.query('SELECT * FROM products WHERE product_name=?', [addInventory], function(err, rows, fields) {
+							  if (err) throw err;
+							 
+							  console.log('The solution is: ', rows);
+							});
+						})
+			});
+			
+}
+
+
+
+
+
+
+
+
+
+
+	
